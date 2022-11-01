54a9ee0e50d89a4ce32b7628ec93eb274d2e42fe
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2022-11-01 17:11:35 +0100

    MonkeyPatch functions to test game sequence, add difficulty selection

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -1,35 +1,34 @@
 from tictactoe.cpu_ai import Difficulty
-from tictactoe.game_mechanic import CreateGame
 from tictactoe.tictactoe import TicTacToe, set_difficulty
 from unittest import mock
 
+def mock_play():
+    pass
 
-
-class MockCreateGame(CreateGame):
-    def play(self):
-        pass
-
+@mock.patch('tictactoe.game_mechanic.CreateGame.play', side_effect=mock_play)
 @mock.patch('tictactoe.tictactoe.input', side_effect='q')
-def test_tictactoe_exits_when_player_enters_q(mock_input):
+def test_tictactoe_exits_when_player_enters_q(mock_input, mock_play):
     party = TicTacToe()
-    party = party.start_game(MockCreateGame())
+    party = party.start_game()
 
     assert party == None
     mock_input.assert_called_once()
+    mock_play.assert_called_once()
 
-
+@mock.patch('tictactoe.game_mechanic.CreateGame.play', side_effect=mock_play)
 @mock.patch('tictactoe.tictactoe.input', side_effect= ["e", "q"])
-def test_tictactoe_aks_again_when_player_input_not_valid(mock_input):
+def test_tictactoe_aks_again_when_player_input_not_valid(mock_input, mock_play):
     party = TicTacToe()
-    party = party.start_game(MockCreateGame())
+    party = party.start_game()
 
     assert party == None
     mock_input.assert_called()
 
+@mock.patch('tictactoe.game_mechanic.CreateGame.play', side_effect=mock_play)
 @mock.patch('tictactoe.tictactoe.input', side_effect= ["c", "q"])
-def test_TTT_creates_a_new_game_when_asked_to_continue(mock_input):
+def test_TTT_creates_a_new_game_when_asked_to_continue(mock_input, mock_play):
     party = TicTacToe()
-    party.start_game(MockCreateGame())
+    party.start_game()
     assert not hasattr(party.game, "board")
 
 
@@ -38,11 +37,27 @@ def test_TTC_has_a_difficulty_setting():
     assert isinstance(party.difficulty, Difficulty)
 
 
-def test_set_difficulty_return_difficulty():
-    difficulty = set_difficulty('Easy')
-    assert difficulty == Difficulty['Easy']
+@mock.patch('tictactoe.tictactoe.input', side_effect= ["Easy", "Hard"])
+def test_set_difficulty_return_appropriate_difficulty(mock_input):
+    difficulty = set_difficulty()
+    assert difficulty == Difficulty.Easy
+    difficulty = set_difficulty()
+    assert difficulty == Difficulty.Hard
 
 
-def test_set_difficulty_can_be_parametrized():
-    difficulty = set_difficulty('Hard')
-    assert difficulty == Difficulty['Hard']
+@mock.patch('tictactoe.tictactoe.input', side_effect= ["Eays", 'Mediume', "Hard"])
+def test_set_difficulty_loops_until_appropriate_input(mock_input):
+    difficulty = set_difficulty()
+    assert difficulty == Difficulty.Hard
+
+
+@mock.patch('tictactoe.game_mechanic.CreateGame.play', side_effect=mock_play)
+@mock.patch('tictactoe.tictactoe.input', side_effect= ["d", "q"])
+@mock.patch('tictactoe.tictactoe.set_difficulty', return_value=Difficulty.Hard)
+def test_TTT_can_change_difficulty_after_game(mock_input,
+                                              mock_play,
+                                              mock_difficulty):
+    party = TicTacToe()
+    assert party.difficulty == Difficulty.Medium
+    party.start_game()
+    assert party.difficulty == Difficulty.Hard
--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -11,29 +11,37 @@ class TicTacToe:
         self.player_input = ""
         self.difficulty = difficulty
 
-    def start_game(self, game: CreateGame = CreateGame()) -> None:
-        self.game = game.play()
+    def start_game(self, game_class = CreateGame) -> None:
+        self.game = game_class(self.difficulty).play()
         while self.player_input == "":
 
-            while self.player_input not in ["c", "q"]:
+            while self.player_input not in ["c", "d", "q"]:
                 self.player_input = self.get_player_input()
 
             if self.player_input == "q":
                 return
 
+            if self.player_input == 'd':
+                self.difficulty = set_difficulty()
+
+
             self.player_input = ""
-            self.game = game.play()
+            self.game = game_class(self.difficulty).play()
 
     def get_player_input(self) -> str:
         return input(
             """
             Would you like to play an other game ?
-            Press "c" to continue, press "q" to exit the game.
+            Press "c" to continue
+            Press "q" to exit the game.
+            Press "d" to change the difficulty"
             """
         )
 
-def  set_difficulty(level='') -> Difficulty:
-
+def  set_difficulty() -> Difficulty:
+        level  = ''
+        while level not in Difficulty.__members__:
+            level = input()
         return Difficulty[level]
 
 
