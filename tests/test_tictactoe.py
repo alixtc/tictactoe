9f0e8e0141e9559909d6ff3bbdb30941dab9f0da
Alix Tiran-Cappello <alix.tiran-cappello@renault.com>
2022-11-01 15:59:15 +0100

    refactor test using patchs and mock not manual mocking

--- tests/test_tictactoe.py
+++ tests/test_tictactoe.py
@@ -1,51 +1,35 @@
 from tictactoe.cpu_ai import Difficulty
 from tictactoe.game_mechanic import CreateGame
 from tictactoe.tictactoe import TicTacToe, set_difficulty
+from unittest import mock
+
 
 
 class MockCreateGame(CreateGame):
     def play(self):
         pass
 
-
-def test_tictactoe_exits_when_player_enters_q():
+@mock.patch('tictactoe.tictactoe.input', side_effect='q')
+def test_tictactoe_exits_when_player_enters_q(mock_input):
     party = TicTacToe()
-
-    def mock_input() -> str:
-        return "q"
-
-    party.get_player_input = mock_input
     party = party.start_game(MockCreateGame())
 
     assert party == None
+    mock_input.assert_called_once()
 
 
-def test_tictactoe_aks_again_when_player_input_not_valid():
+@mock.patch('tictactoe.tictactoe.input', side_effect= ["e", "q"])
+def test_tictactoe_aks_again_when_player_input_not_valid(mock_input):
     party = TicTacToe()
-
-    ANSWERS = iter(["e", "q"])
-
-    def mock_input() -> str:
-        return ANSWERS.__next__()
-
-    party.get_player_input = mock_input
     party = party.start_game(MockCreateGame())
 
     assert party == None
+    mock_input.assert_called()
 
-
-def test_TTT_creates_a_new_game_when_asked_to_continue():
+@mock.patch('tictactoe.tictactoe.input', side_effect= ["c", "q"])
+def test_TTT_creates_a_new_game_when_asked_to_continue(mock_input):
     party = TicTacToe()
-
-    ANSWERS = iter(["c", "q"])
-
-    def mock_input():
-        return ANSWERS.__next__()
-
-    party.get_player_input = mock_input
-
     party.start_game(MockCreateGame())
-
     assert not hasattr(party.game, "board")
 
 
